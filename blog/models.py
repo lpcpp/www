b7044d85ce8694c8ba68e49cf8195121a0ba793e
lp_cpp@163.com <luopeng@luopengdeMacBook-Air.local>
2014-09-16 23:30:49 +0800

    create models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+class Blog(models.Model):
+    caption = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey(Category)
