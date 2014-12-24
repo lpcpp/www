3b8b68c24e6e2d37ad8d30329bc210259b3a2fa9
lp_cpp@163.com <luopeng@luopengdeMacBook-Air.local>
2014-12-24 15:03:59 +0800

    [add]增加用户注册功能
    1.用户注册和登录添加验证码
    2.用户注册需要邮件激活

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 #!-*-coding:utf-8-*-
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -12,23 +13,22 @@ class Category(models.Model):
         return self.name
 
 
+#class User(models.Model):
+#    username = models.CharField(max_length=50)
+#    password = models.CharField(max_length=50)
+#    email = models.EmailField()
+
+#    def __unicode__(self):
+#        return self.username
+        
+
 class Blog(models.Model):
     id = models.AutoField(primary_key=True)
     caption = models.CharField(max_length=200)
     content = models.TextField()
     category = models.ForeignKey(Category)
+    user = models.ForeignKey(User)
     tm = models.DateTimeField('发表时间', auto_now_add=True)
 
     def __unicode__(self):
         return self.caption
-
-
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return self.username
-
-
-
---
+++ blog/verifycode.py
@@ -0,0 +1,89 @@
+#!-*-coding:utf-8-*-
+import random
+import string
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+from django.http import HttpResponse
+from www.settings import VERIFY_CODE_TTF
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
+
+
+def create_verify_code():
+    print 'enter create_verify_code'
+    chars = string.letters + string.digits
+    size = (120, 30)
+    img_type = "GIF"
+    mode = "RGB"
+    bg_color = (255, 255, 255)
+    fg_color = (0, 0, 255)
+    font_size = 18
+    font_type = VERIFY_CODE_TTF
+    length = 4
+    draw_lines = True
+    n_line = (1,2)
+    draw_points = True
+    point_chance = 2
+
+    width, height =size
+    img = Image.new(mode, size, bg_color)
+    draw = ImageDraw.Draw(img)
+
+    def get_chars():
+        return random.sample(chars, length)
+
+    def create_lines():
+        line_num = random.randint(*n_line)
+        begin = (random.randint(0, size[0]), random.randint(0, size[1]))
+        end = (random.randint(0, size[0]), random.randint(0, size[1]))
+        draw.line([begin, end], fill=(0, 0, 0))
+
+    def create_points():
+        chance = min(100, max(0, int(point_chance)))
+        #print chance
+        for w in xrange(width):
+            for h in xrange(height):
+                tmp = random.randint(0, 100)
+                if tmp > 100 - chance:
+                    draw.point((w, h), fill=(0, 0, 0))
+
+    def create_strs():
+        c_chars = get_chars()
+        #print c_chars
+        strs = ' %s ' % ' '.join(c_chars)
+        #print strs
+        font = ImageFont.truetype(font_type, font_size)
+        font_width, font_height = font.getsize(strs)
+        draw.text(((width - font_width) / 3, (height - font_height) / 3), strs,
+                    font=font, fill=fg_color)
+        return ''.join(c_chars) 
+
+    if draw_lines:
+        create_lines()
+    if draw_points:
+        create_points()
+    strs = create_strs()
+    params = [1 - float(random.randint(1, 2)) / 100,
+              0,
+              0,
+              0,
+              1 - float(random.randint(1, 10)) / 100,
+              float(random.randint(1, 2)) / 500,
+              0.001,
+              float(random.randint(1, 2)) / 500
+              ]
+    img = img.transform(size, Image.PERSPECTIVE, params)
+    img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+    return img, strs
+
+
+def verify_code(request):
+    print 'enter verify_code'
+    mstream = StringIO.StringIO()
+    img, code = create_verify_code()
+    print code
+    img.save(mstream, "GIF")
+    request.session['verifycode'] = code
+    
+    return HttpResponse(mstream.getvalue(), "image/gif")
